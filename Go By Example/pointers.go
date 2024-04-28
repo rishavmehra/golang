289d6fa171010e55b8576999528239165ecf53a6
Rishav Mehra <rishavmehra61@gmail.com>
2024-04-28 23:04:09 +0530

    more in depth on pointers

--- Go By Example/pointers.go
+++ Go By Example/pointers.go
@@ -28,4 +28,24 @@ func main() {
 	var ptr *int // declaring a pointer to an integer
 	ptr = &num   //assigning the address of 'num' to 'ptr'
 	// fmt.Println(ptr)
+
 }
+
+// They are the pointer denotations. The ‘*’ is a pointer to a variable, and ‘&’ is its memory address.
+
+// int a; /* Declares a variable of type integer */
+// int *b; /* Declares a pointer to type integer */
+// int &c; /* Not possible */
+
+// a = 10; /* a gets the value of 10 */
+// b = 10; /* not possible */
+// b = &a; /* b gets the address of a */
+// *b = 20; /* a now has the value 20 */
+
+// Since the asterisk resembles a gold coin, I read *b as “value of b”.
+// Ampersand is also pronounced as ‘et’ which is close enough to “at”, so I read &a as “at a”
+
+// You can set a value for the pointer, but only once you've asked for memory for it using "new". This is how your code should look
+
+// b = new int; /* you create a new integer variable where *b points */
+// *b = 20; /* b now has the value 20 */
